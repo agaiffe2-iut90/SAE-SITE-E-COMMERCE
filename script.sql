27c39a5477edfaa73310ad1faecf78f7ce171365
sugdenaz <sugdenaz.ekici@edu.univ-fcomte.fr>
2024-01-26 20:50:05 +0100

    article client

--- script.sql
+++ script.sql
@@ -3,7 +3,7 @@ DROP TABLE IF EXISTS ligne_commande;
 DROP TABLE IF EXISTS commande;
 DROP TABLE IF EXISTS etat;
 DROP TABLE IF EXISTS parfum;
-DROP TABLE IF EXISTS type_parfum;
+
 DROP TABLE IF EXISTS genre;
 DROP TABLE IF EXISTS volume;
 DROP TABLE IF EXISTS utilisateur;
@@ -18,6 +18,7 @@ CREATE TABLE utilisateur(
     est_actif tinyint(1),
     PRIMARY KEY (id_utilisateur)
 );
+/*ENGINE=InnoDB DEFAULT CHARSET utfmb4;*/
 
 CREATE TABLE genre(
                       id_genre INT AUTO_INCREMENT,
@@ -47,25 +48,23 @@ CREATE TABLE commande(
                          FOREIGN KEY(etat_id) REFERENCES etat(id_etat)
 )DEFAULT CHARSET utf8mb4;
 
-CREATE TABLE type_parfum(
-                       id_type_parfum INT AUTO_INCREMENT,
-                       marque VARCHAR(255),
-                       PRIMARY KEY(id_parfum)
-)DEFAULT CHARSET utf8mb4;
+
 
 CREATE TABLE parfum(
-                       id_parfum INT AUTO_INCREMENT,
-                       nom_parfum VARCHAR(255),
-                       prix_parfum DOUBLE,
-                       volume_id INT,
-                       type_parfum_id INT,
-                       description VARCHAR(255),
-                       fournisseur VARCHAR(255),
-                       marque VARCHAR(255),
-                       image VARCHAR(255),
-                       PRIMARY KEY(id_parfum),
-                       FOREIGN KEY(volume_id) REFERENCES volume(id_volume),
-                       FOREIGN KEY(type_parfum_id) REFERENCES genre(id_genre)
+    id_parfum INT AUTO_INCREMENT,
+    nom_parfum VARCHAR(255),
+    prix_parfum DOUBLE,
+    volume_id INT,
+    type_parfum_id INT,
+    conditionnement VARCHAR(255),
+    description VARCHAR(255),
+    fournisseur VARCHAR(255),
+    marque VARCHAR(255),
+    stock INT,
+    image VARCHAR(255),
+    PRIMARY KEY(id_parfum),
+    FOREIGN KEY(volume_id) REFERENCES volume(id_volume),
+    FOREIGN KEY(type_parfum_id) REFERENCES genre(id_genre)
 )DEFAULT CHARSET utf8mb4;
 
 
@@ -113,12 +112,12 @@ INSERT INTO genre(nom_genre) VALUES
 
 INSERT INTO etat(libelle) VALUES ('en attente'), ('expédié'), ('validé'), ('confirmé');
 
-INSERT INTO parfum( nom_parfum, prix_parfum, volume_id, type_parfum_id, description, fournisseur, marque ,image) VALUES
-                    ('batman', 30.0, 2, 4, 'batman parfum', 'italie', 'naverde', 'batman.jpeg'),
-                    ('licorne', 19.0, 1, 4, 'licorne parfum', 'netball', 'air-val', 'licorne.jpeg'),
-                    ('spiderman', 20.0, 2, 4, 'spiderman parfum', 'marvel', 'marvel', 'spiderman.jpeg'),
-                    ('tresor', 50.0, 2, 1, 'tresor parfum', 'china', 'jsp', 'tresor.jpg'),
-                    ('working girl', 255.0, 3, 1, 'girl parfum', 'jspp', 'working', 'working_girl_100ml.jpg');
+INSERT INTO parfum( nom_parfum, prix_parfum, volume_id, type_parfum_id, conditionnement, description, fournisseur, marque, stock ,image) VALUES
+                    ('batman', 30.0, 2, 4, 'flacon plastique','batman parfum', 'italie', 'naverde', 5,'batman.jpeg'),
+                    ('licorne', 19.0, 1, 4,'flacon plastique', 'licorne parfum', 'netball', 'air-val',7,'licorne.jpeg'),
+                    ('spiderman', 20.0, 2, 4, 'flacon plastique','spiderman parfum', 'marvel', 'marvel',122, 'spiderman.jpeg'),
+                    ('tresor', 50.0, 2, 1, 'flacon verre','tresor parfum', 'china', 'jsp',777, 'tresor.jpg'),
+                    ('working girl', 255.0, 3, 1,'flacon verre', 'girl parfum', 'jspp', 'working',2, 'working_girl_100ml.jpg');
 
 INSERT INTO commande( date_achat , utilisateur_id , etat_id ) VALUES
                     ('2015-02-02', 2, 1),
